4686af904dba904b3de4c0f9df2177da437713cb
Sabrina Bosco <sabrina@wonderweblabs.io>
2016-10-24 22:15:43 +0200

    setup satellizer

--- src/app/auth/auth.controller.js
+++ src/app/auth/auth.controller.js
@@ -1,3 +1,12 @@
 export class AuthController {
 
+  contructor($auth) {
+    'ngInject';
+
+    this.$auth = $auth
+  }
+
+  register() {
+    this.$auth.signup({email: 'sabrina.bosco89@gmail.com'});
+  }
 }
